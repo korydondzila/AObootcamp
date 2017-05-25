90720797d7cb11833b7fbe711e22597280843ed9
Kory Dondzila <korydondzila@gmail.com>
2017-05-25 11:52:49 -0400

    Replaced JS with TypeScript and updates files to reflect that change.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SlideShow from './components/slideShow'
-import './index.css'
-
-ReactDOM.render(
-	<SlideShow />, document.getElementById('root')
-);
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import SlideShow from './components/slideShow';
+import './index.css';
+
+ReactDOM.render(
+    <SlideShow />, document.getElementById('root')
+);
